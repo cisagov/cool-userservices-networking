6e66bb846e36c39391a7b49c501f0328efe5c8be
David Redmin <david.redmin@trio.dhs.gov>
2021-03-02 10:04:30 -0500

    Add remote states for Terraform and Users accounts

--- remote_states.tf
+++ remote_states.tf
@@ -16,6 +16,8 @@ data "terraform_remote_state" "master" {
     key            = "cool-accounts/master.tfstate"
   }
 
+  # There is only one environment for this account, so there is
+  # no need to match the current Terraform workspace.
   workspace = "production"
 }
 
@@ -47,6 +49,40 @@ data "terraform_remote_state" "sharedservices_networking" {
   }
 
   workspace = local.workspace_type
+}
+
+data "terraform_remote_state" "terraform" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-accounts/terraform.tfstate"
+  }
+
+  # There is only one environment for this account, so there is
+  # no need to match the current Terraform workspace.
+  workspace = "production"
+}
+
+data "terraform_remote_state" "users" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-accounts/users.tfstate"
+  }
+
+  # There is only one environment for this account, so there is
+  # no need to match the current Terraform workspace.
+  workspace = "production"
 }
 
 data "terraform_remote_state" "userservices" {
